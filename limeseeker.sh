2403a12967b45a65afff140445093e31c3ddca11
LimeMec <markus.carlsson@outlook.com>
2026-01-06 15:49:45 +0100

    ändrat i kod

--- limeseeker.sh
+++ limeseeker.sh
@@ -2,15 +2,15 @@
 
 BASE_DIR="$(cd "$(dirname "$0")" && pwd)"
 
-#-------------------
+#------------------
 # Säkerställ sudo
-# ------------------
+# -----------------
 source "$BASE_DIR/lib/privileges.sh"
 require_sudo "$@"
 
-#--------------------------------------------
+#------------------------------------------
 # Skapa katalog och fil för loggrapporten
-# -------------------------------------------
+# -----------------------------------------
 REPORT_DIR="$BASE_DIR/reports"
 mkdir -p "$REPORT_DIR"
 export REPORT_FILE="$REPORT_DIR/LimeSeeker_$(date +%Y%m%d_%H%M%S).txt"
@@ -20,9 +20,9 @@ export REPORT_FILE="$REPORT_DIR/LimeSeeker_$(date +%Y%m%d_%H%M%S).txt"
 #--------------------
 exec > >(tee -a "$REPORT_FILE") 2>&1
 
-#--------------------
+#-------------------
 # Rubrik för logg
-# -------------------
+# ------------------
 echo "==========================================================="
 echo "      LimeSeeker Report: $(date)"
 echo "==========================================================="
@@ -36,9 +36,9 @@ source "$BASE_DIR/lib/utils.sh"
 source "$BASE_DIR/lib/ui.sh"
 source "$BASE_DIR/lib/menu.sh"
 
-#--------------------
+#----------------
 # Ladda moduler
-#--------------------
+#----------------
 shopt -s nullglob
 for module in "$BASE_DIR"/modules/*.sh; do
     source "$module"
